bce2a1162bb3c047a813d1501a87b2b546190f9b
Sonika Kumar <sonikakmr2468@gmail.com>
2017-01-16 15:13:24 +1300

    List the movies

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-$(document).ready(function() {
+var app = new Vue ({
+  el: '.content',
 
-    // Your code goes in here    
+  data: {
+    movies: []
+  },
 
-});
+  created: function() {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: function() {
+      var self = this;
+      var list = '/api/';
+
+      fetch(list).then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        self.movies = json.movies;
+      });
+    }
+  }
+})
